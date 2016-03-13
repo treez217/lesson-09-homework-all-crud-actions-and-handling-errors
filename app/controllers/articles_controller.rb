64fdfda9b7a25537be3c71d5e3c441d7af64d8b9
Derrick Chou <treez217@gmail.com>
2016-03-13 19:35:18 -0400

    Finished Lesson 9 HW

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+
   def index
     @articles = Article.all
   end
@@ -12,16 +13,40 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.create(article_params)
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Successfully Created Article"
     redirect_to articles_path
+    else
+      flash.now[:error] = "Error Creating Article, Title & Body field must be entered"
+      render :new
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_path(@article), notice: "Successfully Updated Article"
+    else
+      flash.now[:error] = "Error Updating Article, Title & Body field must be entered"
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path, notice: "Successfully Delete Article"
   end
 
   private 
   def article_params
     params.require(:article).permit(:title, :body)
   end
-
-
 
 
 end
